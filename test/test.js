4734fae81517655ecc05b4eeddf456a201f14cb5
shriyashshukla <harishukla797@gmail.com>
2023-07-31 16:30:11 +0530

    js

---
+++ test/test.js
@@ -0,0 +1,19 @@
+console.log("helooo");
+let a =67;
+console.log(a);
+a="shriyash";
+console.log(a);
+
+let b ="67";
+let c ="hari";
+console.log(b+c);
+console.log(typeof(b+c));
+
+const d={
+    name:"harish",
+    age:25,
+    city:"hyderabad"
+    
+};
+d['friend']="hari";
+console.log(d);
